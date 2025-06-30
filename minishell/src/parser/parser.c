aed08063916139991f24628b64ca7d6b3d1175ec
Samet Can Incesu <saincesu@k1m14s04.42kocaeli.com.tr>
2025-06-30 18:17:48 +0300

    parser updated

--- minishell/src/parser/parser.c
+++ minishell/src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: saincesu <saincesu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/29 16:31:49 by saincesu          #+#    #+#             */
-/*   Updated: 2025/06/29 18:08:27 by saincesu         ###   ########.fr       */
+/*   Updated: 2025/06/30 18:17:20 by saincesu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,12 @@ void	parser(t_shell *shell, t_parser *parsed)
 {
 	t_token *a;
 	int i;
+	int j;
 	int flag_count;
 	int arg_count;
 
 	i = 0;
+	j = 0;
 	flag_count = 0;
 	arg_count = 0;
 	a = shell->args;
@@ -36,40 +38,30 @@ void	parser(t_shell *shell, t_parser *parsed)
 	}
 	parsed->flags = ft_alloc(sizeof(char *) * (flag_count + 1));
 	parsed->args = ft_alloc(sizeof(char *) * (arg_count + 1));
+	parsed->input = NULL;
+	parsed->output = NULL;
 	a = shell->args;
+	
 	if (a && a->content)
 	{
 		parsed->command = a->content;
 		a = a->next;
 	}
-	while (a && a->content && a->content[0] == '-')
+	while (a && a->content)
 	{
-		parsed->flags[i++] = ft_strdup(a->content);
+		if (ft_strncmp(a->content, ">", 1) == 0 && a->next && a->next->content)
+			parsed->output = ft_strdup(a->next->content);
+		else if (ft_strncmp(a->content, ">>", 2) == 0 && a->next && a->next->content)
+			parsed->output = ft_strdup(a->next->content);
+		else if (ft_strncmp(a->content, "<", 1) == 0 && a->next && a->next->content)
+			parsed->input = ft_strdup(a->next->content);
+		else if (a->content[0] == '-')
+			parsed->flags[i++] = ft_strdup(a->content);
+		else if (!is_operator(a->content[0])) // bu satıra bak > b yazınca b'yi de argsa atıyor
+			parsed->args[j++] = ft_strdup(a->content);
+		// else if heredoc yazcam.
 		a = a->next;
 	}
 	parsed->flags[i] = NULL;
-	i = 0;
-	while (a && a->content && !is_operator(a->content[0]))
-	{
-		parsed->args[i++] = ft_strdup(a->content);
-		a = a->next;
-	}
-	parsed->args[i] = NULL;
+	parsed->args[j] = NULL;
 }
-
-
-
-
-
-
-
-
-/*void	parser(t_shell *shell, t_parser *parsed)
-{
-	if (shell->args->content)
-		shell->args->content = parsed->command;
-	if (shell->args->next->content && shell->args->next->content[0] == '-')
-		shell->args->content = parsed->flags;
-	else if (shell->args->next->content)
-		shell->args->next->content = parsed->
-}*/
