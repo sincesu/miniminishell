55c93fc6982d89a75c0e9e4ae55d9155379562bb
Samet Can Incesu <saincesu@k1m15s03.42kocaeli.com.tr>
2025-07-04 19:42:17 +0300

    syntax error files created and finished

---
+++ minishell/src/syntax_error/quote_error.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   quote_error.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saincesu <saincesu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/04 19:41:07 by saincesu          #+#    #+#             */
+/*   Updated: 2025/07/04 19:41:26 by saincesu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+#include "../../Libft/libft.h"
+
+int	quote_error(char *input, t_shell *shell)
+{
+	int i;
+	char quote;
+
+	i = 0;
+	while (input[i])
+	{
+		if (input[i] == '\'' || input[i] == '"')
+		{
+			quote = input[i++];
+			while (input[i] && input[i] != quote)
+				i++;
+			if (input[i] != quote)
+			{
+				printf("minishell: syntax error: unmatched quote\n");
+				shell->exit_code = 2;
+				return (1);
+			}
+		}
+		if (input[i]) //check
+			i++;
+	}
+	return (0);
+}
+
+int	operator_error(int c, t_shell *shell)
+{
+	if (c == '|')
+	{
+		printf("minishell: syntax error near unexpected token '|'\n");
+		shell->exit_code = 2;
+		return (1);
+	}
+	return (0);
+}
---
+++ minishell/src/syntax_error/syntax_error.c
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   syntax_error.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saincesu <saincesu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/04 19:41:09 by saincesu          #+#    #+#             */
+/*   Updated: 2025/07/04 19:41:10 by saincesu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+#include "../../Libft/libft.h"
+
+int	syntax_error(t_shell *shell)
+{
+	t_token *a;
+	char	*str;
+
+	a = shell->args;
+	while (a && a->content)
+	{
+		if(is_operator(a->content[0]))
+		{
+			if (a->type == R_APPEND)
+				if (r_append_error(a, shell))
+					return (1);
+			if (a->type == R_HERE)
+				if (r_here_error(a, shell))
+					return (1);
+			if (a->type == R_OUT || a->type == R_IN)
+				if(r_in_and_r_out_error(a, shell))
+					return (1);
+			if (a->type == PIPE)
+				if (pipe_error(str, a, shell))
+					return (1);
+		}
+		str = a->content;
+		a = a->next;
+	}
+	return (0);
+}
+
+int	r_append_error(t_token *a, t_shell *shell)
+{
+	if (a->type == R_APPEND)
+	{
+		if (!a->next)
+		{
+			printf("minishell: syntax error near unexpected token 'newline'\n");
+			shell->exit_code = 2;
+			return (1);
+		}
+		if (is_operator(a->next->content[0]))
+		{
+			printf("minishell: syntax error near unexpected token `>>'\n");
+			shell->exit_code = 2;
+			return (1);
+			}
+		}
+		return (0);
+}
+
+int	r_here_error(t_token *a, t_shell *shell)
+{
+	if (!a->next)
+		{
+			printf("minishell: syntax error near unexpected token 'newline'\n");
+			shell->exit_code = 2;
+			return (1);
+		}
+		if (is_operator(a->next->content[0]))
+		{
+			printf("minishell: syntax error near unexpected token `<<'\n");
+			shell->exit_code = 2;
+			return (1);
+		}
+		return (0);
+}
+
+int	r_in_and_r_out_error(t_token *a, t_shell *shell)
+{
+	if (!a->next)
+	{
+		printf("minishell: syntax error near unexpected token 'newline'\n");
+		shell->exit_code = 2;
+		return (1);
+	}
+	return (0);
+}
+
+int	pipe_error(char *str, t_token *a, t_shell *shell)
+{
+	if (!a->next)
+	{
+		printf("minishell: syntax error: unexpected end of file\n");
+		shell->exit_code = 2;
+		return (1);
+	}
+	if (is_operator(str[0]))
+	{
+		printf("bash: syntax error near unexpected token '|'\n");
+		shell->exit_code = 2;
+		return (1);
+	}
+	else if (a->next->type == PIPE)
+	{
+		printf("bash: syntax error near unexpected token '|'\n");
+		shell->exit_code = 2;
+		return (1);
+	}
+	return (0);
+}
