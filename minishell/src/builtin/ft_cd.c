7e9a4c7fcd96bd11632e96fc8d2385949842ac26
Mert Özcan <iammertozcan@gmail.com>
2025-07-08 12:33:25 +0300

    Update ft_cd

--- minishell/src/builtin/ft_cd.c
+++ minishell/src/builtin/ft_cd.c
@@ -10,31 +10,101 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
-#include "../../include/minishell.h"
+#include "../../Libft/libft.h"
+
+char	*ft_handle_home_path(const char *path)
+{
+	char	*home;
+	char	*target;
+	size_t	len;
+	size_t	i;
+
+	home = getenv("HOME");
+	if (!home)
+	{
+		ft_putstr_fd("cd: HOME not set\n", 2);
+		return (NULL);
+	}
+	if (!path || path[0] == '\0' || (path[0] == '~' && path[1] == '\0'))
+		return (home);
+	len = ft_strlen(home) + ft_strlen(path + 1) + 1;
+	target = malloc(len);
+	if (!target)
+	{
+		perror("malloc");
+		return (NULL);
+	}
+	i = 0;
+	while (home[i])
+	{
+		target[i] = home[i];
+		i++;
+	}
+	while (path[++i - ft_strlen(home)])
+		target[i - 1] = path[i - ft_strlen(home)];
+	target[i - 1] = '\0';
+	return (target);
+}
+
+char	*ft_handle_oldpwd(void)
+{
+	char	*oldpwd;
+
+	oldpwd = getenv("OLDPWD");
+	if (!oldpwd)
+	{
+		ft_putstr_fd("cd: OLDPWD not set\n", 2);
+		return (NULL);
+	}
+	ft_putstr_fd(oldpwd, 1);
+	ft_putchar_fd('\n', 1);
+	return (oldpwd);
+}
+
+char	*ft_get_target_path(const char *path, int *needs_free)
+{
+	if (!path || path[0] == '\0' || (path[0] == '~' && path[1] == '\0'))
+		return (ft_handle_home_path(path));
+	else if (path[0] == '-' && path[1] == '\0')
+		return (ft_handle_oldpwd());
+	else if (path[0] == '~' && path[1] == '/')
+	{
+		*needs_free = 1;
+		return (ft_handle_home_path(path));
+	}
+	return ((char *)path);
+}
 
 void	ft_cd(const char *path)
 {
-	//char	*cwd;
+	char	*target;
+	char	*cwd;
+	int		needs_free;
 
-	if (path == NULL)
-		path = getenv("HOME");
-	if (chdir(path) == -1)
+	needs_free = 0;
+	target = ft_get_target_path(path, &needs_free);
+	if (!target)
+		return ;
+	cwd = getcwd(NULL, 0);
+	if (!cwd)
 	{
-		perror("cd");
+		perror("getcwd");
+		if (needs_free)
+			free(target);
 		return ;
 	}
-	// cwd = ft_alloc(4096);
-	// if (!cwd)
-	// {
-	// 	perror("malloc");
-	// 	return ;
-	// }
-	// if (getcwd(cwd, 4096))
-	// {}
-	// else
-	// 	perror("getcwd");
-	// free(cwd);
+	setenv("OLDPWD", cwd, 1);
+	free(cwd);
+	if (chdir(target) == -1)
+	{
+		ft_putstr_fd("cd: ", 2);
+		ft_putstr_fd(target, 2);
+		ft_putstr_fd(": ", 2);
+		perror("");
+	}
+	if (needs_free)
+		free(target);
 }
