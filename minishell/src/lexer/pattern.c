639b6ea559cfb94bcd0f5fabd153db19d354f095
Samet Can Incesu <saincesu@k1m23s03.42kocaeli.com.tr>
2025-07-09 13:42:14 +0300

    is_operator_type added

--- minishell/src/lexer/pattern.c
+++ minishell/src/lexer/pattern.c
@@ -6,7 +6,7 @@
 /*   By: saincesu <saincesu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/31 04:00:39 by saincesu          #+#    #+#             */
-/*   Updated: 2025/06/10 16:58:20 by saincesu         ###   ########.fr       */
+/*   Updated: 2025/07/09 13:41:59 by saincesu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ int	is_whitespace(char c)
 	return (c == ' ' || c == '\t' || c == '\n');
 }
 
-int skip_whitespace(char *str)
+int	skip_whitespace(char *str)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (is_whitespace(str[i]))
@@ -30,6 +30,12 @@ int skip_whitespace(char *str)
 int	is_operator(char c)
 {
 	return (c == '>' || c == '<' || c == '|');
+}
+
+int	is_operator_type(int type)
+{
+	return (type == R_OUT || type == R_APPEND
+		|| type == R_IN || type == R_HERE);
 }
 
 int	is_quote(char c)
