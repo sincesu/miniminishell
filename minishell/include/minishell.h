48e0855bc7cb9e7d4677823caf6f24d620ab1e7e
Samet Can Incesu <saincesu@k2m01s06.42kocaeli.com.tr>
2025-07-10 12:01:15 +0300

    parser added

--- minishell/include/minishell.h
+++ minishell/include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: saincesu <saincesu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 15:58:24 by saincesu          #+#    #+#             */
-/*   Updated: 2025/07/04 19:41:36 by saincesu         ###   ########.fr       */
+/*   Updated: 2025/07/10 11:54:59 by saincesu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,50 +15,51 @@
 
 typedef enum e_token_type
 {
-	R_OUT = 1,	// > redirection out
-	R_APPEND,	// >>
-	PIPE,		// |
-	R_IN,		// < redirection in
-	R_HERE,		// << heredoc
-	S_WORD,		// tek tırnak
-	D_WORD,		// çift tırnak
-	U_WORD,		// tırnaksız
+	R_OUT = 1,
+	R_APPEND,
+	PIPE,
+	R_IN,
+	R_HERE,
+	S_WORD,
+	D_WORD,
+	U_WORD,
 }	t_token_type;
 
 typedef struct s_mem_list
 {
 	void				*block;
 	struct s_mem_list	*next;
-} 	t_mem_list;
+}	t_mem_list;
 
 typedef struct s_token
 {
 	char			*content;
-	int				type; //t_token_type olarak değiştirilebilir
+	int				type;
 	int				last;
 	struct s_token	*next;
 }	t_token;
 
+typedef struct s_redirect
+{
+	char	*file_name;
+	int		type;
+	int		flags;
+	char	*document;
+}	t_redirect;
+
 typedef struct s_parser
 {
-	char	*command;
-	char	**flags;
-	char	**args;
-	char	*input;
-	char	*output;
+	char			**args;
+	t_redirect		*redirect;
+	int				redirect_count;
+	int				fd_in;
+	int				fd_out;
+	struct s_parser	*prev;
+	struct s_parser	*next;
 }	t_parser;
-
-/*
-1-) 0. argüman kesinlikle komut
-2-) 1. argüman ve sonrasında gelenlerden başında - olanlar komut
-3-) 1. argümandan sonra gelen ve başında - olmayanlar input (| > >> < <<) olmadığı sürece
-4-) 2. argümandan sonra gelen ve | olan bir token görürse önceki argümanı input olarak alıcak 
-eğer sağında argüman yoksa outputu stdout'a verecek, argüman varsa argümana input olarak verecek,
-*/
 
 typedef struct s_shell
 {
-	//t_list	*collector; //burasına ihtiyaç duymayacağım.
 	char	*input;
 	char	*old_input;
 	t_token	*args;
@@ -66,11 +67,10 @@ typedef struct s_shell
 	int		exit_code;
 }	t_shell;
 
-
 char		**lexer_split(const	char *s);
 t_token		*lexer(char *input);
 void		expander(t_shell *shell);
-t_mem_list	**get_head_mem();
+t_mem_list	**get_head_mem(void);
 int			is_whitespace(char c);
 int			skip_whitespace(char *str);
 int			is_operator(char c);
@@ -79,10 +79,11 @@ int			create_operator(char *str, t_token **head_list);
 int			get_word_type(char c);
 int			create_word(char *str, t_token **head_list);
 int			create_delimn(char *str, t_token **head_list);
-void		ft_free();
+void		ft_free(void);
 void		safe_abort(int exit_code);
 void		register_alloc_mem(void *ptr);
 void		*ft_alloc(unsigned long size);
+int			is_operator_type(int type);
 
 int			r_append_error(t_token *a, t_shell *shell);
 int			r_here_error(t_token *a, t_shell *shell);
@@ -92,14 +93,13 @@ int			syntax_error(t_shell *shell);
 int			operator_error(int c, t_shell *shell);
 int			quote_error(char *input, t_shell *shell);
 
-void	ft_cd(const char *path);
-int		ft_pwd(void);
-int		ft_echo(char **args);
-void	ft_exit(t_shell *shell);
-void	ft_env(char **env_copy);
+void		ft_cd(const char *path);
+int			ft_pwd(void);
+int			ft_echo(char **args);
+void		ft_exit(t_shell *shell);
+void		ft_env(char **env_copy);
 
-void	parser(t_shell *shell, t_parser *parsed);
-
+t_parser	*parser(t_token	*token);
 int			env_len(char **env);
 char		**copy_env(char **env);
 
@@ -114,11 +114,10 @@ void		ft_handle_sigint(int signum);
 void		ft_handle_sigquit(int signum);
 void		ft_init_signals(void);
 
-//executer
-void    ft_execute_commands(t_shell shell);
-int		ft_shell_commands(t_shell *shell);
-void	ft_one_command(t_shell shell);
-void	ft_multi_command(t_shell shell);
-char	**token_list_to_argv(t_token *tokens);
+void		ft_execute_commands(t_shell shell);
+int			ft_shell_commands(t_shell *shell);
+void		ft_one_command(t_shell shell);
+void		ft_multi_command(t_shell shell);
+char		**token_list_to_argv(t_token *tokens);
 
 #endif
