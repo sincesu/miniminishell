15435899d81d8a0fa589856555e2883ca554dcdb
Samet Can Incesu <saincesu@k1m21s01.42kocaeli.com.tr>
2025-07-15 16:55:43 +0300

    norm fixed

--- minishell/src/builtin/ft_exit.c
+++ minishell/src/builtin/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: saincesu <saincesu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 12:53:24 by saincesu          #+#    #+#             */
-/*   Updated: 2025/06/29 14:07:30 by saincesu         ###   ########.fr       */
+/*   Updated: 2025/07/15 13:03:23 by saincesu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,50 +16,51 @@
 #include "../../include/minishell.h"
 #include <stdio.h>
 
-static int ft_check_param(char *str)
+static int	ft_check_param(char *str)
 {
-    int i = 0;
-    
-    if (!str)
-        return (1);
-    if (str[0] == '-' || str[0] == '+')
-        i = 1;
-    if (str[i] == '\0')
-        return (1);
-    while (str[i])
-    {
-        if (str[i] < '0' || str[i] > '9')
-            return (1);
-        i++;
-    }
-    return (0);
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (1);
+	if (str[0] == '-' || str[0] == '+')
+		i = 1;
+	if (str[i] == '\0')
+		return (1);
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
-void ft_exit(t_shell *shell)
+void	ft_exit(t_shell *shell)
 {
-    int exit_code;
-    
-    if (shell->args == NULL || shell->args->next == NULL)
-    {
-        printf("exit\n");
-        safe_abort(shell->exit_code);
-        return;
-    }
-    if (ft_check_param(shell->args->next->content))
-    {
-        printf("exit\n");
-        printf("minishell: exit: %s: numeric argument required\n",
-               shell->args->next->content);
-        safe_abort(shell->exit_code);
-        return;
-    }
-    if (shell->args->next->next)
-    {
-        printf("exit\n");
-        printf("minishell: exit: too many arguments\n");
-        return ;
-    }
-    exit_code = ft_atoi(shell->args->next->content);
-    printf("exit\n");
-    safe_abort(exit_code);
+	int	exit_code;
+
+	if (shell->args == NULL || shell->args->next == NULL)
+	{
+		printf("exit\n");
+		safe_abort(shell->exit_code);
+		return ;
+	}
+	if (ft_check_param(shell->args->next->content))
+	{
+		printf("exit\n");
+		printf("minishell: exit: %s: numeric argument required\n",
+			shell->args->next->content);
+		safe_abort(shell->exit_code);
+		return ;
+	}
+	if (shell->args->next->next)
+	{
+		printf("exit\n");
+		printf("minishell: exit: too many arguments\n");
+		return ;
+	}
+	exit_code = ft_atoi(shell->args->next->content);
+	printf("exit\n");
+	safe_abort(exit_code);
 }
