84282050f440972dad1d8dcedc2679ba02b334c3
Samet Can Incesu <saincesu@k1m21s01.42kocaeli.com.tr>
2025-07-15 20:54:19 +0300

    norm fixed

--- minishell/src/executer/ft_builtin_utils.c
+++ minishell/src/executer/ft_builtin_utils.c
@@ -1,35 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_builtin_utils.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saincesu <saincesu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/15 20:51:22 by saincesu          #+#    #+#             */
+/*   Updated: 2025/07/15 20:52:54 by saincesu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../include/minishell.h"
 #include "../../Libft/libft.h"
 #include <stdio.h>
 
-int ft_is_builtin(char *cmd)
+int	ft_is_builtin(char *cmd)
 {
-    if (!cmd)
-        return 0;
-    return (ft_strncmp(cmd, "echo", 5) == 0 ||
-            ft_strncmp(cmd, "cd", 3) == 0 ||
-            ft_strncmp(cmd, "pwd", 4) == 0 ||
-            ft_strncmp(cmd, "export", 7) == 0 ||
-            ft_strncmp(cmd, "unset", 6) == 0 ||
-            ft_strncmp(cmd, "env", 4) == 0 ||
-            ft_strncmp(cmd, "exit", 5) == 0);
+	if (!cmd)
+		return (0);
+	return (ft_strncmp(cmd, "echo", 5) == 0
+		|| ft_strncmp(cmd, "cd", 3) == 0
+		|| ft_strncmp(cmd, "pwd", 4) == 0
+		|| ft_strncmp(cmd, "export", 7) == 0
+		|| ft_strncmp(cmd, "unset", 6) == 0
+		|| ft_strncmp(cmd, "env", 4) == 0
+		|| ft_strncmp(cmd, "exit", 5) == 0);
 }
 
-int ft_execute_builtin(t_shell *shell, t_exec_unit *unit)
+int	ft_execute_builtin(t_shell *shell, t_exec_unit *unit)
 {
-    if (ft_strncmp(unit->args[0], "echo", 5) == 0)
-        return (ft_echo(unit->args));
-    else if (ft_strncmp(unit->args[0], "cd", 3) == 0)
-        return (ft_cd(unit->args[1]));
-    else if (ft_strncmp(unit->args[0], "pwd", 4) == 0)
-        return (ft_pwd());
-    else if (ft_strncmp(unit->args[0], "export", 7) == 0)
-        return (ft_export(shell));
-    else if (ft_strncmp(unit->args[0], "unset", 6) == 0)
-        return (ft_unset(shell));
-    else if (ft_strncmp(unit->args[0], "env", 4) == 0)
-        return (ft_env(shell->env));
-    else if (ft_strncmp(unit->args[0], "exit", 5) == 0)
-        ft_exit(shell);
-    return (1);
+	if (ft_strncmp(unit->args[0], "echo", 5) == 0)
+		return (ft_echo(unit->args));
+	else if (ft_strncmp(unit->args[0], "cd", 3) == 0)
+		return (ft_cd(unit->args[1]));
+	else if (ft_strncmp(unit->args[0], "pwd", 4) == 0)
+		return (ft_pwd());
+	else if (ft_strncmp(unit->args[0], "export", 7) == 0)
+		return (ft_export(shell));
+	else if (ft_strncmp(unit->args[0], "unset", 6) == 0)
+		return (ft_unset(shell));
+	else if (ft_strncmp(unit->args[0], "env", 4) == 0)
+		return (ft_env(shell->env));
+	else if (ft_strncmp(unit->args[0], "exit", 5) == 0)
+		ft_exit(shell);
+	return (1);
 }
--- minishell/src/executer/ft_execute_commands.c
+++ minishell/src/executer/ft_execute_commands.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_execute_commands.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saincesu <saincesu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/15 20:53:01 by saincesu          #+#    #+#             */
+/*   Updated: 2025/07/15 20:53:16 by saincesu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../include/minishell.h"
 #include "../../Libft/libft.h"
 #include <stddef.h>
@@ -19,7 +31,7 @@ int	ft_is_multi_command(t_shell shell)
 	return (0);
 }
 
-void    ft_execute_commands(t_shell *shell, t_parser parsed)
+void	ft_execute_commands(t_shell *shell, t_parser parsed)
 {
 	if (ft_is_multi_command(*shell))
 		shell->exit_code = ft_multi_command(shell, parsed);
