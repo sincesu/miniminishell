9b3831c9b96d8a30bb7612d1e3fa7672090d37dd
Mert Ozcan <merozcan@k2m04s01.42kocaeli.com.tr>
2025-07-18 12:51:08 +0300

    Fix norm mistakes of minishell.h

--- minishell/include/minishell.h
+++ minishell/include/minishell.h
@@ -154,14 +154,15 @@ int			ft_apply_redirections(t_redirect *redir);
 int			ft_is_builtin(char *cmd);
 int			ft_execute_builtin(t_shell *shell, t_parser *parsed);
 char		*ft_search_command_path(char *command);
-char		*ft_get_heredoc_input(const char *delimiter, t_shell *shell, t_parser parsed);
+char		*ft_get_heredoc_input(const char *delimiter,
+				t_shell *shell, t_parser parsed);
 
 // parser
 t_parser	ft_parse_command(t_shell *shell, t_parser parsed);
 t_redirect	*ft_process_redirects(t_shell *shell, t_parser parser);
-void	ft_initialize_parser(t_parser *parser, t_parser parsed);
-void	ft_count_args_redirects(t_token *tokens, int *arg_count, 
-		int *redirect_count);
-void	ft_process_args(t_shell *shell, t_parser *parser);
+void		ft_initialize_parser(t_parser *parser, t_parser parsed);
+void		ft_count_args_redirects(t_token *tokens, int *arg_count,
+				int *redirect_count);
+void		ft_process_args(t_shell *shell, t_parser *parser);
 
 #endif
