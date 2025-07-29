260c49760f1b6723e18a3bcee99a01259d29e464
Samet Can Incesu <saincesu@k1m23s01.42kocaeli.com.tr>
2025-07-29 20:45:00 +0300

    multiple arguments blocked

--- minishell/src/main.c
+++ minishell/src/main.c
@@ -6,7 +6,7 @@
 /*   By: saincesu <saincesu@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 20:35:09 by saincesu          #+#    #+#             */
-/*   Updated: 2025/07/23 19:22:41 by saincesu         ###   ########.fr       */
+/*   Updated: 2025/07/29 20:29:40 by saincesu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,8 +65,9 @@ int	main(int ac, char **av, char **env)
 {
 	t_shell	shell;
 
-	(void)ac;
 	(void)av;
+	if (ac != 1)
+		return (1);
 	ft_bzero(&shell, sizeof(t_shell));
 	shell.env = copy_env(env);
 	shell.env = update_shell_lvl(shell.env);
@@ -80,5 +81,4 @@ int	main(int ac, char **av, char **env)
 		ft_process_shell_input(&shell);
 	}
 	safe_abort(0);
-	return (0);
 }
