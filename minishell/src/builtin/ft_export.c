4b4164dcf4abea599e79c2b4fcdb9e530dd8e888
Samet Can Incesu <saincesu@k1m23s01.42kocaeli.com.tr>
2025-07-29 20:47:10 +0300

    printing with bubble sort && unused function deleted

--- minishell/src/builtin/ft_export.c
+++ minishell/src/builtin/ft_export.c
@@ -6,21 +6,13 @@
 /*   By: saincesu <saincesu@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/14 19:13:03 by saincesu          #+#    #+#             */
-/*   Updated: 2025/07/23 19:22:41 by saincesu         ###   ########.fr       */
+/*   Updated: 2025/07/29 20:34:00 by saincesu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 #include "../../Libft/libft.h"
 #include <stdio.h>
-
-static int	ft_export_error(char *err)
-{
-	ft_putstr_fd("minishell: export: -", 2);
-	ft_putchar_fd(err[1], 2);
-	ft_putendl_fd(": invalid option", 2);
-	return (2);
-}
 
 static int	check_export_arg(t_shell *shell, char *arg)
 {
@@ -35,25 +27,62 @@ static int	check_export_arg(t_shell *shell, char *arg)
 	return (0);
 }
 
-static void	export_list_printer(t_shell *shell)
+static int	merge_env_and_export_list(char **dst, char **env, char **export)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (env && env[j])
+	{
+		if (env[j][0] != '_')
+			dst[i++] = env[j];
+		j++;
+	}
+	j = 0;
+	while (export && export[j])
+		dst[i++] = export[j++];
+	dst[i] = NULL;
+	return (0);
+}
+
+static void	print_sorted_list(char **combined)
 {
 	int		i;
 	char	*eq;
 
 	i = 0;
-	while (shell->env && shell->env[i])
+	while (combined[i])
 	{
-		eq = ft_strchr(shell->env[i], '=');
+		eq = ft_strchr(combined[i], '=');
 		if (eq)
-			printf("declare -x %.*s=\"%s\"\n", (int)(eq - shell->env[i]),
-				shell->env[i], eq + 1);
+			printf("declare -x %.*s=\"%s\"\n", (int)(eq - combined[i]),
+				combined[i], eq + 1);
 		else
-			printf("declare -x %s\n", shell->env[i]);
+			printf("declare -x %s\n", combined[i]);
 		i++;
 	}
+}
+
+static void	export_list_printer(t_shell *shell)
+{
+	int		env_len;
+	int		exp_len;
+	int		i;
+	char	**combined;
+
+	env_len = 0;
+	exp_len = 0;
 	i = 0;
-	while (shell->export_only_list && shell->export_only_list[i])
-		printf("declare -x %s\n", shell->export_only_list[i++]);
+	while (shell->env && shell->env[env_len])
+		env_len++;
+	while (shell->export_only_list && shell->export_only_list[exp_len])
+		exp_len++;
+	combined = ft_alloc(sizeof(char *) * (env_len + exp_len + 1));
+	merge_env_and_export_list(combined, shell->env, shell->export_only_list);
+	bubble_sort(combined);
+	print_sorted_list(combined);
 }
 
 int	ft_export(t_shell *shell)
@@ -61,12 +90,8 @@ int	ft_export(t_shell *shell)
 	t_token	*token;
 
 	token = shell->args;
-	if (token->next && token->next->type == U_WORD)
-	{
-		if (token->next->content[0] == '-')
-			return (ft_export_error(shell->args->next->content));
-	}
-	if (!token->next)
+	if (!token->next || (token->next
+			&& is_operator_token(token->next->content)))
 	{
 		export_list_printer(shell);
 		return (0);
@@ -74,6 +99,8 @@ int	ft_export(t_shell *shell)
 	token = token->next;
 	while (token)
 	{
+		if (is_operator_token(token->content))
+			break ;
 		if (check_export_arg(shell, token->content))
 			return (1);
 		if (handle_export_arg(shell, token->content))
