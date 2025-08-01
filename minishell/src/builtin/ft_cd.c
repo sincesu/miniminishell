857af8c19aae45b5015dcae7791de461c5617884
Samet Can Incesu <saincesu@k1m22s01.42kocaeli.com.tr>
2025-08-01 19:24:12 +0300

    Invalid read error fixed

--- minishell/src/builtin/ft_cd.c
+++ minishell/src/builtin/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: saincesu <saincesu@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 12:53:19 by saincesu          #+#    #+#             */
-/*   Updated: 2025/07/31 18:40:23 by saincesu         ###   ########.fr       */
+/*   Updated: 2025/08/01 19:23:23 by saincesu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,13 +48,13 @@ static char	*ft_get_target_path(t_shell *shell, t_parser *parsed)
 {
 	char	*old_pwd;
 
+	if (parsed->args[1] == NULL)
+		return (ft_resolve_home_path(shell, "~"));
 	if (parsed->args[2] != NULL)
 	{
 		ft_perror("cd", NULL, "too many arguments");
 		return (NULL);
 	}
-	if (parsed->args[1] == NULL)
-		return (ft_resolve_home_path(shell, "~"));
 	if (parsed->args[1][0] == '~' && parsed->args[1][1] == '/')
 		return (ft_resolve_home_path(shell, parsed->args[1]));
 	else if (parsed->args[1][0] == '-' && parsed->args[1][1] == '\0')
