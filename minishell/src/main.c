31abafe10cd13d75c25056546486416e108de465
Samet Can Incesu <saincesu@k2m07s01.42kocaeli.com.tr>
2025-08-06 15:26:20 +0300

    unnecessary lines removed

--- minishell/src/main.c
+++ minishell/src/main.c
@@ -6,7 +6,7 @@
 /*   By: saincesu <saincesu@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/15 20:35:09 by saincesu          #+#    #+#             */
-/*   Updated: 2025/08/04 16:18:13 by saincesu         ###   ########.fr       */
+/*   Updated: 2025/08/05 13:54:55 by saincesu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int	ft_get_shell_input(t_shell *shell)
+static void	ft_get_shell_input(t_shell *shell)
 {
 	ft_init_signals(PROMPT);
 	shell->old_input = readline("minishell> ");
@@ -25,43 +25,33 @@ int	ft_get_shell_input(t_shell *shell)
 	{
 		ft_putendl_fd("exit", 1);
 		safe_abort(0);
-		return (0);
 	}
 	shell->input = ft_strdup(shell->old_input);
 	free(shell->old_input);
-	if (!shell->input)
-	{
-		safe_abort(0);
-		return (0);
-	}
-	return (1);
 }
 
 void	ft_process_shell_input(t_shell *shell)
 {
 	t_parser	*parsed;
 
-	if (*shell->input)
-	{
-		add_history(shell->input);
-		if (quote_error(shell->input, shell)
-			|| operator_error(shell->input[0], shell))
-			return ;
-		shell->args = lexer(shell->input);
-		if (!shell->args)
-			return ;
-		if (syntax_error(shell))
-			return ;
-		expander(shell);
-		merger(shell->args);
-		set_underscore_env(shell->args, shell);
-		parsed = parser(shell->args);
-		if (!shell->args)
-			return ;
-		if (handle_heredoc(shell, parsed))
-			return ;
-		ft_execute_commands(shell, parsed);
-	}
+	add_history(shell->input);
+	if (quote_error(shell->input, shell)
+		|| operator_error(shell->input[0], shell))
+		return ;
+	shell->args = lexer(shell->input);
+	if (!shell->args)
+		return ;
+	if (syntax_error(shell))
+		return ;
+	expander(shell);
+	merger(shell->args);
+	set_underscore_env(shell->args, shell);
+	parsed = parser(shell->args);
+	if (!shell->args)
+		return ;
+	if (handle_heredoc(shell, parsed))
+		return ;
+	ft_execute_commands(shell, parsed);
 }
 
 int	main(int ac, char **av, char **env)
@@ -75,12 +65,9 @@ int	main(int ac, char **av, char **env)
 	shell.env = copy_env(env);
 	shell.env = update_shell_lvl(shell.env);
 	shell.exit_code = 0;
-	if (!shell.env)
-		return (1);
 	while (1)
 	{
-		if (!ft_get_shell_input(&shell))
-			break ;
+		ft_get_shell_input(&shell);
 		ft_process_shell_input(&shell);
 	}
 	safe_abort(0);
