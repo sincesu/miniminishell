8c1581cecda0b21e613428f5a1343f7f94f8b03f
Samet Can Incesu <saincesu@k1m23s01.42kocaeli.com.tr>
2025-08-07 23:18:36 +0300

    renamed parameters to a more meaningful name

--- minishell/src/syntax_error/syntax_error.c
+++ minishell/src/syntax_error/syntax_error.c
@@ -6,7 +6,7 @@
 /*   By: saincesu <saincesu@student.42kocaeli.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 19:41:09 by saincesu          #+#    #+#             */
-/*   Updated: 2025/08/07 19:00:29 by saincesu         ###   ########.fr       */
+/*   Updated: 2025/08/07 23:17:17 by saincesu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 #include "../../Libft/libft.h"
 #include <stdio.h>
 
-static int	r_append_error(t_token *a, t_shell *shell)
+static int	r_append_error(t_token *token, t_shell *shell)
 {
-	if (a->type == R_APPEND)
+	if (token->type == R_APPEND)
 	{
-		if (!a->next)
+		if (!token->next)
 		{
 			ft_putstr_fd("minishell: syntax error near unexpected token"
 				" 'newline'\n", 2);
 			shell->exit_code = 2;
 			return (1);
 		}
-		if (is_operator(a->next->content[0]))
+		if (is_operator(token->next->content[0]))
 		{
 			ft_putstr_fd("minishell: syntax error near unexpected token"
 				"`>>'\n", 2);
@@ -36,16 +36,16 @@ static int	r_append_error(t_token *a, t_shell *shell)
 	return (0);
 }
 
-static int	r_here_error(t_token *a, t_shell *shell)
+static int	r_here_error(t_token *token, t_shell *shell)
 {
-	if (!a->next)
+	if (!token->next)
 	{
 		ft_putstr_fd("minishell: syntax error near unexpected token"
 			" 'newline'\n", 2);
 		shell->exit_code = 2;
 		return (1);
 	}
-	if (is_operator(a->next->content[0]))
+	if (is_operator(token->next->content[0]))
 	{
 		ft_putstr_fd("minishell: syntax error near unexpected token `<<'\n", 2);
 		shell->exit_code = 2;
@@ -54,9 +54,9 @@ static int	r_here_error(t_token *a, t_shell *shell)
 	return (0);
 }
 
-static int	r_in_and_r_out_error(t_token *a, t_shell *shell)
+static int	r_in_and_r_out_error(t_token *token, t_shell *shell)
 {
-	if (!a->next)
+	if (!token->next)
 	{
 		ft_putstr_fd("minishell: syntax error near unexpected token"
 			" 'newline'\n", 2);
@@ -66,9 +66,9 @@ static int	r_in_and_r_out_error(t_token *a, t_shell *shell)
 	return (0);
 }
 
-static int	pipe_error(char *str, t_token *a, t_shell *shell)
+static int	pipe_error(char *str, t_token *token, t_shell *shell)
 {
-	if (!a->next)
+	if (!token->next)
 	{
 		ft_putstr_fd("minishell: syntax error: unexpected end of file\n", 2);
 		shell->exit_code = 2;
@@ -80,7 +80,7 @@ static int	pipe_error(char *str, t_token *a, t_shell *shell)
 		shell->exit_code = 2;
 		return (1);
 	}
-	else if (a->next->type == PIPE)
+	else if (token->next->type == PIPE)
 	{
 		ft_putstr_fd("minishell: syntax error near unexpected token '|'\n", 2);
 		shell->exit_code = 2;
